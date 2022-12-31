13b5568e6fc87a3a96e434f3c374d7ed227078f3
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-12-31 15:46:34 +0530

    added files

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const navbar = document.getElementById('navbar')
 const menu = document.querySelector('.menu')
 const close = document.querySelector('.close');
 const navLinks = document.querySelector('.navLinks')
-
+const moreWork = document.getElementById('more__work__btn')
+const moreWorkSection = document.getElementById('more__work__section')
 window.addEventListener('scroll', (e)=>{
     navbar.classList.add('scrollNav')
     if (scrollY === 0) {
@@ -15,4 +16,8 @@ menu.addEventListener('click',()=>{
 })
 close.addEventListener('click',()=>{
     navLinks.classList.remove('show')
+})
+
+moreWork.addEventListener('click',()=>{
+
 })
